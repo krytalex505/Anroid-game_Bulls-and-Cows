ebf497b0510bcba93f253285b8d38aa0a28a1762
krytalex505 <alexkryt055@gmail.com>
2026-08-12 15:05:18 +0300

    Add files via upload

---
+++ app/src/test/java/com/example/bullsandcows/GameApplication.kt
@@ -0,0 +1,11 @@
+package com.example.bullsandcows
+
+import org.junit.Test
+import org.junit.Assert.*
+
+class GameApplicationTest {
+    @Test
+    fun testApplication() {
+        assertTrue(true)
+    }
+}
